4eb9d3e633a14a9876ab793295daf1e7e5f8c1d8
Lin <gy6543721@yahoo.com.tw>
2025-02-18 12:17:22 +0900

    Compose Refactor

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.compose.compiler)
     alias(libs.plugins.kotlin.android)
 }
 
@@ -27,22 +28,28 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
 }
 
 dependencies {
-    implementation(libs.gson)
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
+    implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.activity)
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.material3)
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.gson)
     testImplementation(libs.junit)
+    implementation(libs.material)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
--- app/src/main/java/levi/lin/wifianalyser/activities/MainActivity.kt
+++ app/src/main/java/levi/lin/wifianalyser/activities/MainActivity.kt
@@ -1,108 +1,99 @@
 package levi.lin.wifianalyser.activities
 
+import android.Manifest
+import android.content.Context
 import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
-import android.net.wifi.WifiManager
-import android.os.Bundle
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import android.Manifest
 import android.net.Uri
 import android.net.wifi.ScanResult
-import androidx.core.app.ActivityCompat
-import android.widget.Button
-import android.widget.TextView
+import android.net.wifi.WifiManager
+import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import levi.lin.wifianalyser.R
 import levi.lin.wifianalyser.data.DataStorage
 import java.time.Instant
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
 
-class MainActivity : AppCompatActivity(), LocationListener {
-    private lateinit var dataStorage : DataStorage
-
-    private lateinit var consoleTextView: TextView
-    private lateinit var exportButton: Button
-
+class MainActivity : ComponentActivity(), LocationListener {
+    private lateinit var dataStorage: DataStorage
     private lateinit var wifiManager: WifiManager
     private lateinit var locationManager: LocationManager
-
     private lateinit var exportLauncher: ActivityResultLauncher<String>
+
+    private val permissionRequest = registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+        if (isGranted) {
+            startLocationUpdates()
+        } else {
+            Toast.makeText(this, R.string.permission_denied, Toast.LENGTH_LONG).show()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
         dataStorage = DataStorage(this)
-
-        wifiManager = applicationContext.getSystemService(WIFI_SERVICE) as WifiManager
+        wifiManager = getSystemService(WIFI_SERVICE) as WifiManager
         locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
 
-        consoleTextView = findViewById(R.id.consoleTextView)
-
-        setupExportButton()
-
-        if (checkPermissions()) {
-            startLocationUpdates()
-        } else {
-            requestPermissions()
-        }
-    }
-
-    private fun setupExportButton() {
-        exportButton = findViewById(R.id.exportButton)
-        exportLauncher =
-            registerForActivityResult(ActivityResultContracts.CreateDocument("application/json")) { uri: Uri? ->
-                uri?.let {
-                    val jsonRecords = dataStorage.toJson()
-                    jsonRecords.let {
-                        contentResolver.openOutputStream(uri)?.bufferedWriter()
-                            .use { writer -> writer?.write(jsonRecords) }
-                        Toast.makeText(this, R.string.data_exported, Toast.LENGTH_LONG)
-                            .show()
-                    }
+        exportLauncher = registerForActivityResult(ActivityResultContracts.CreateDocument("application/json")) { uri: Uri? ->
+            uri?.let {
+                val jsonRecords = dataStorage.toJson()
+                jsonRecords.let {
+                    contentResolver.openOutputStream(uri)?.bufferedWriter().use { writer -> writer?.write(jsonRecords) }
+                    Toast.makeText(this, R.string.data_exported, Toast.LENGTH_LONG).show()
                 }
             }
-
-        exportButton.setOnClickListener {
-            exportLauncher.launch("wifi_map_data.json")
         }
-    }
 
-    private fun checkPermissions(): Boolean {
-        return ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
-                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions()
+        } else {
+            startLocationUpdates()
+        }
+
+        setContent {
+            WifiAnalyzerApp(
+                context = this,
+                dataStorage = dataStorage,
+                exportLauncher = exportLauncher,
+                wifiManager = wifiManager,
+                locationManager = locationManager
+            )
+        }
     }
 
     private fun requestPermissions() {
         ActivityCompat.requestPermissions(
             this,
-            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_WIFI_STATE),
+            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_WIFI_STATE),
             100
         )
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == 100) {
-            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                startLocationUpdates()
-            } else {
-                Toast.makeText(this, getString(R.string.permission_denied), Toast.LENGTH_LONG).show()
-            }
-        }
-    }
 
     private fun startLocationUpdates() {
         val providers = locationManager.allProviders
@@ -124,7 +115,6 @@ class MainActivity : AppCompatActivity(), LocationListener {
             lastKnownLocation?.let { location ->
                 scanWifi(location.latitude, location.longitude)
             }
-
         } else {
             Toast.makeText(this, getString(R.string.no_gps_location_provider_available), Toast.LENGTH_LONG).show()
         }
@@ -138,41 +128,97 @@ class MainActivity : AppCompatActivity(), LocationListener {
         val wifiScanResults = if (ActivityCompat.checkSelfPermission(
                 this,
                 Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
             ) != PackageManager.PERMISSION_GRANTED
         ) {
+            permissionRequest.launch("No Permission")
             return
         } else {
             wifiManager.scanResults
         }
 
         dataStorage.addOrUpdateCluster(latitude, longitude, wifiScanResults)
-
-        appendClusterToConsole(latitude, longitude, wifiScanResults)
     }
+}
 
-    private val dateFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").withZone(ZoneId.systemDefault())
+@Composable
+fun WifiAnalyzerApp(
+    context: Context,
+    dataStorage: DataStorage,
+    exportLauncher: ActivityResultLauncher<String>,
+    wifiManager: WifiManager,
+    locationManager: LocationManager
+) {
+    var consoleText by remember { mutableStateOf("") }
 
-    private fun appendClusterToConsole(lat: Double, lon: Double, scanResults: List<ScanResult>) {
-        val sb = StringBuilder()
-        sb.append("----------------------------------------------------\n")
-        sb.append("[${getString(R.string.networksCaptured)}]: ${dataStorage.getNetworksCount()}\n")
-        sb.append("[${getString(R.string.lastUpdateTime)}]: ${dateFormatter.format(Instant.now())}\n")
-        sb.append("[${getString(R.string.gps)}]\n$lat, $lon\n")
-        sb.append("[${getString(R.string.networks)} (${scanResults.size})]\n")
-        sb.append(String.format("%-15s %-15s %-15s %-15s\n", getString(R.string.ssid), getString(R.string.level), getString(R.string.security), getString(R.string.frequency)))
-        sb.append("----------------------------------------------------\n")
-
-        for (result in scanResults.sortedByDescending { it.level }) {
-            val securityType = when {
-                result.capabilities.contains("WEP") -> "WEP"
-                result.capabilities.contains("WPA2") -> "WPA2"
-                result.capabilities.contains("WPA") -> "WPA"
-                else -> getString(R.string.open)
+    LaunchedEffect(key1 = dataStorage) {
+        withContext(Dispatchers.IO) {
+            val wifiScanResults = if (ActivityCompat.checkSelfPermission(
+                    context,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                    context,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                return@withContext
+            } else {
+                wifiManager.scanResults
             }
 
-            sb.append(String.format("%-15s %-15s %-15s %-15s\n", result.wifiSsid.toString().trim(), result.level.toString().trim(), securityType.toString().trim(), result.frequency.toString().trim()))
+            val lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+            lastKnownLocation?.let { location ->
+
+                dataStorage.addOrUpdateCluster(location.latitude, location.longitude, wifiScanResults)
+                consoleText = buildConsoleText(dataStorage, location.latitude, location.longitude, wifiScanResults)
+            }
+        }
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize().safeDrawingPadding(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(20.dp)
+    ) {
+        Text(
+            modifier = Modifier.padding(start = 15.dp),
+            text = consoleText,
+            textAlign = TextAlign.Start
+        )
+
+        Button(
+            onClick = {
+                exportLauncher.launch("wifi_map_data.json")
+            },
+            content = {
+                Text(text = stringResource(id = R.string.export_data))
+            }
+        )
+    }
+}
+
+private fun buildConsoleText(dataStorage: DataStorage, lat: Double, lon: Double, scanResults: List<ScanResult>): String {
+    val sb = StringBuilder()
+    sb.append("--------------------------------------------------------------------------\n")
+    sb.append("[Networks Captured]: ${dataStorage.getNetworksCount()}\n")
+    sb.append("[Last Update Time]: ${DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").withZone(ZoneId.systemDefault()).format(Instant.now())}\n")
+    sb.append("[GPS]\n$lat, $lon\n")
+    sb.append("[Networks (${scanResults.size})]\n")
+    sb.append(String.format("%-20s %-15s %-15s %-15s\n", "SSID", "Level", "SecurityType", "Frequency"))
+    sb.append("--------------------------------------------------------------------------\n")
+
+    for (result in scanResults.sortedByDescending { it.level }) {
+        val securityType = when {
+            result.capabilities.contains("WEP") -> "WEP"
+            result.capabilities.contains("WPA2") -> "WPA2"
+            result.capabilities.contains("WPA") -> "WPA"
+            else -> "Open"
         }
 
-        consoleTextView.text = sb.toString()
+        sb.append(String.format("%-20s %-15s %-15s %-15s\n", result.SSID, result.level.toString().trim(), securityType.toString().trim(), result.frequency.toString().trim()))
     }
+
+    return sb.toString()
 }
